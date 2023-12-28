16f097cbc22fbca7754520038590d3607fb62944
Mostafa-ElHawary <alhwarymostafa@gmail.com>
2023-12-28 17:05:30 +0200

    added some style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,43 +10,43 @@ import GenderComponent from "@/app/component/Gender";
 export default function Home() {
   return (
     <>
-      <div className="Name p-4">
+        <div className="Name p-4 bg-gray-100">
         {/* NAME Section */}
         <section id="nameSection" className="mb-8">
-          <h2 className="text-xl font-bold">NAME :</h2>
-          <p id="p-NAME" className="mt-2">
+          <h2 className="text-2xl font-semibold text-blue-700">NAME :</h2>
+          <p id="p-NAME" className="mt-2 text-gray-800">
             <Names />
           </p>
         </section>
 
         {/* STATUS Section */}
         <section id="statusSection" className="mb-8">
-          <h2 className="text-xl font-bold">STATUS :</h2>
-          <p id="p-STUTAS" className="mt-2">
+          <h2 className="text-2xl font-semibold text-blue-700">STATUS :</h2>
+          <p id="p-STUTAS" className="mt-2 text-gray-800">
             <Stats />
           </p>
         </section>
 
         {/* PHONE Section */}
         <section id="phoneSection" className="mb-8">
-          <h2 className="text-xl font-bold">PHONE :</h2>
-          <p id="p-PHONE" className="mt-2">
+          <h2 className="text-2xl font-semibold text-blue-700">PHONE :</h2>
+          <p id="p-PHONE" className="mt-2 text-gray-800">
             <Phones />
           </p>
         </section>
 
         {/* CITY Section */}
         <section id="citySection" className="mb-8">
-          <h2 className="text-xl font-bold">CITY :</h2>
-          <p id="p-CITY" className="mt-2">
+          <h2 className="text-2xl font-semibold text-blue-700">CITY :</h2>
+          <p id="p-CITY" className="mt-2 text-gray-800">
             <CityComponent />
           </p>
         </section>
 
         {/* GENDER Section */}
         <section id="genderSection" className="mb-8">
-          <h2 className="text-xl font-bold">GENDER :</h2>
-          <p id="p-GENDER" className="mt-2">
+          <h2 className="text-2xl font-semibold text-blue-700">GENDER :</h2>
+          <p id="p-GENDER" className="mt-2 text-gray-800">
             <GenderComponent />
           </p>
         </section>
