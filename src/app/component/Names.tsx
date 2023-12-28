d6ca95295979461d6ce1c302835308fe6eebedf8
Mostafa-ElHawary <alhwarymostafa@gmail.com>
2023-12-28 17:03:15 +0200

    added some style

--- src/app/component/Names.tsx
+++ src/app/component/Names.tsx
@@ -1,208 +1,208 @@
 "use client";;
 import React, { useEffect } from 'react';
 
+const data = `صفاء,ديب
+Mahitab,Mohamed
+Marian,Rafat
+Wafaa,Abd El-Raheem
+Ebram,Philip
+Asmaa,Fathy
+Mai,Abdul Qader
+Dina,Adel
+أحمد,عبدالكريم
+Vena,Hana
+Tota,Bota
+MaHamd,Samy
+Esraa,Rashad
+Beshoy,Gamel
+Souad,Elsrogy
+Souad,Elsrogy
+Nevin,Hammam
+Nevin,Hammam
+Om Abd Elrahman,Ahmed
+Losy,Hader
+Sara,Mahmoud Al Husmdy
+Sara,Mahmoud Al Husmdy
+Walaa,Assad
+Semsma,Semsem
+Mero,Ezzat
+Sue,Saleh
+Gharam,Saleh
+Gharam,Saleh
+Layaly,Egy
+Manal,Mohamed
+Mero,Gaber
+Karyma,Hassan
+Shry,Aly
+Rana,Kamel
+Kholoud,Abdo
+Kholoud,Abdo
+Shrook,Ashraf
+Shrook,Ashraf
+Eveline,Meshel
+Eveline,Meshel
+Ola,Farghaly
+YøYø,ÃTëf
+Shams,Esmail
+Mariam,Ashraf
+Asmaa,Ebraheem
+Fatma,Ahmed
+Doaa,Hamed
+Amira,Sayed
+Amira,Sayed
+Aziza,Ahmed
+Samar,Mohamed
+Hala,Sayed
+Amal,Mostafa
+Nour,Osama
+Sma,Yousef
+Ahmed,Ramadan
+Ahmed,Ramadan
+Ahmed,Ramadan
+Ahmed,Ramadan
+Haidy,Raafat
+Asmaa,Atef
+GiGi,Remon
+GiGi,Remon
+Sama,Hamdy
+Sama,Hamdy
+Maha,Mehoo
+Mahmoud,Kasem
+اسيل,الحياه
+Sara,Emad
+Hassan,Ayman
+Hassan,Ayman
+گبریہائہہی,اآآسہطورھہہ
+Marwa,Salah
+Marwa,Salah
+Merna,Refaat
+Merna,Refaat
+رحاب,عمر
+Hazem,Moftah
+Esraa,Elsaid
+Esraa,Elsaid
+Esraa,Elsaid
+Esraa,Elsaid
+Esraa,Elsaid
+Esraa,Elsaid
+Esraa,Elsaid
+Rofida,Hashem
+رحيق,الجنه
+Hager,Sayed
+Hager,Sayed
+نسرين احمد,بخيت
+Ash,Gan
+Roaa,Ahmed
+Roaa,Ahmed
+Aya,Mohamed
+Aya,Mohamed
+Kholoud,Saad
+Kholoud,Saad
+Norhan,Fathy
+Mero,Samir
+Mero,Samir
+Karem,Faroq
+Asia,Mohsen
+Mohmed,Elrahman
+Roma,Boktor
+Roma,Boktor
+Maha,Mohammed
+Maha,Mohammed
+Karam,Ahmed
+Koka,Mohamed
+Ñourhan,Ķamal
+Ñourhan,Ķamal
+Nadia,Ali
+Esraa,Esmail
+Nour,Mahmoud
+نور,الرحمن
+Nada,Mohamed
+Nada,Mohamed
+Shimaa,Said
+حمادة جمال,جمال عبدالناصر
+Shimaa,Arafa
+Shimaa,Arafa
+Eman,Ibrahim
+Ahmed,Talaat
+Ahmed,Talaat
+Hadeer,Mahmoud
+شادي,شيكو
+شادي,شيكو
+Yomna,Mohamed
+Hanen,Mostfa
+Om,Farida
+Nädä,Emäd
+Rehab,Fathy
+Twaby,Sud
+Amal,Radwan
+Ahmed,Helles
+Hasnaa,Mostafa
+Hasnaa,Mostafa
+Nada,Ahmed Mohamed
+مفيش,فايده
+Qamar,Essam
+Asmaa,Nabil
+Manar,Bassam
+Madona,Atef
+تنه,ورنه
+Hend,Ahmed
+Tota,Khalid
+Salma,Mostafa
+Salma,Mostafa
+Ana,Yose Ana
+Asmaa,Sdiek
+هبه محمد,بدر
+Karas,Fathy
+Maha,Hamdy
+Maha,Hamdy
+Ayat,Mustafa
+Walaa,El Kady
+Omneya,Ahmed
+مﭑمي,گﭑريگﭑ
+Rehab,Abdou
+Rehab,Abdou
+Youssef,Shenouda
+Noha,Hassan
+Noha,Hassan
+Sara,Moustafa
+Rehab,Musaa
+Shahy,Sarhan
+Gannat,Moustafa
+Marwa,Ibrahiam
+Mohamad,Hany
+Sally,Mahmoud
+Noor,Mohamed
+Rana,Hamdy
+NoUrhan,HaShem
+Sara,Adel
+Andy,Shenoda
+Andy,Shenoda
+Somaya,Emed
+Rasha,Basha
+Rasha,Basha
+Logy,Waleed
+Roka,S. Zaki
+Hanan,Nona
+Sa,MoRa
+Śĥì,Màà
+مها,اروي
+Dina,Alaa
+Aya,Ahmed
+Aya,Ahmed
+Amna,G. Mahmoud
+Maria,Khaled
+Has,Na
+Has,Na
+Eman,Hakim
+Rahma,Seleim
+Nada,FaRghali
+Nermeen,Faried
+Marmora,Boles
+Khaled,Mohamed
+Yassin Abdelrazek,Ahmed
+Ahmed,Hegazy`;
+
 const NameComponent: React.FC = () => {
-  const data = `صفاء,ديب
-  Mahitab,Mohamed
-  Marian,Rafat
-  Wafaa,Abd El-Raheem
-  Ebram,Philip
-  Asmaa,Fathy
-  Mai,Abdul Qader
-  Dina,Adel
-  أحمد,عبدالكريم
-  Vena,Hana
-  Tota,Bota
-  MaHamd,Samy
-  Esraa,Rashad
-  Beshoy,Gamel
-  Souad,Elsrogy
-  Souad,Elsrogy
-  Nevin,Hammam
-  Nevin,Hammam
-  Om Abd Elrahman,Ahmed
-  Losy,Hader
-  Sara,Mahmoud Al Husmdy
-  Sara,Mahmoud Al Husmdy
-  Walaa,Assad
-  Semsma,Semsem
-  Mero,Ezzat
-  Sue,Saleh
-  Gharam,Saleh
-  Gharam,Saleh
-  Layaly,Egy
-  Manal,Mohamed
-  Mero,Gaber
-  Karyma,Hassan
-  Shry,Aly
-  Rana,Kamel
-  Kholoud,Abdo
-  Kholoud,Abdo
-  Shrook,Ashraf
-  Shrook,Ashraf
-  Eveline,Meshel
-  Eveline,Meshel
-  Ola,Farghaly
-  YøYø,ÃTëf
-  Shams,Esmail
-  Mariam,Ashraf
-  Asmaa,Ebraheem
-  Fatma,Ahmed
-  Doaa,Hamed
-  Amira,Sayed
-  Amira,Sayed
-  Aziza,Ahmed
-  Samar,Mohamed
-  Hala,Sayed
-  Amal,Mostafa
-  Nour,Osama
-  Sma,Yousef
-  Ahmed,Ramadan
-  Ahmed,Ramadan
-  Ahmed,Ramadan
-  Ahmed,Ramadan
-  Haidy,Raafat
-  Asmaa,Atef
-  GiGi,Remon
-  GiGi,Remon
-  Sama,Hamdy
-  Sama,Hamdy
-  Maha,Mehoo
-  Mahmoud,Kasem
-  اسيل,الحياه
-  Sara,Emad
-  Hassan,Ayman
-  Hassan,Ayman
-  گبریہائہہی,اآآسہطورھہہ
-  Marwa,Salah
-  Marwa,Salah
-  Merna,Refaat
-  Merna,Refaat
-  رحاب,عمر
-  Hazem,Moftah
-  Esraa,Elsaid
-  Esraa,Elsaid
-  Esraa,Elsaid
-  Esraa,Elsaid
-  Esraa,Elsaid
-  Esraa,Elsaid
-  Esraa,Elsaid
-  Rofida,Hashem
-  رحيق,الجنه
-  Hager,Sayed
-  Hager,Sayed
-  نسرين احمد,بخيت
-  Ash,Gan
-  Roaa,Ahmed
-  Roaa,Ahmed
-  Aya,Mohamed
-  Aya,Mohamed
-  Kholoud,Saad
-  Kholoud,Saad
-  Norhan,Fathy
-  Mero,Samir
-  Mero,Samir
-  Karem,Faroq
-  Asia,Mohsen
-  Mohmed,Elrahman
-  Roma,Boktor
-  Roma,Boktor
-  Maha,Mohammed
-  Maha,Mohammed
-  Karam,Ahmed
-  Koka,Mohamed
-  Ñourhan,Ķamal
-  Ñourhan,Ķamal
-  Nadia,Ali
-  Esraa,Esmail
-  Nour,Mahmoud
-  نور,الرحمن
-  Nada,Mohamed
-  Nada,Mohamed
-  Shimaa,Said
-  حمادة جمال,جمال عبدالناصر
-  Shimaa,Arafa
-  Shimaa,Arafa
-  Eman,Ibrahim
-  Ahmed,Talaat
-  Ahmed,Talaat
-  Hadeer,Mahmoud
-  شادي,شيكو
-  شادي,شيكو
-  Yomna,Mohamed
-  Hanen,Mostfa
-  Om,Farida
-  Nädä,Emäd
-  Rehab,Fathy
-  Twaby,Sud
-  Amal,Radwan
-  Ahmed,Helles
-  Hasnaa,Mostafa
-  Hasnaa,Mostafa
-  Nada,Ahmed Mohamed
-  مفيش,فايده
-  Qamar,Essam
-  Asmaa,Nabil
-  Manar,Bassam
-  Madona,Atef
-  تنه,ورنه
-  Hend,Ahmed
-  Tota,Khalid
-  Salma,Mostafa
-  Salma,Mostafa
-  Ana,Yose Ana
-  Asmaa,Sdiek
-  هبه محمد,بدر
-  Karas,Fathy
-  Maha,Hamdy
-  Maha,Hamdy
-  Ayat,Mustafa
-  Walaa,El Kady
-  Omneya,Ahmed
-  مﭑمي,گﭑريگﭑ
-  Rehab,Abdou
-  Rehab,Abdou
-  Youssef,Shenouda
-  Noha,Hassan
-  Noha,Hassan
-  Sara,Moustafa
-  Rehab,Musaa
-  Shahy,Sarhan
-  Gannat,Moustafa
-  Marwa,Ibrahiam
-  Mohamad,Hany
-  Sally,Mahmoud
-  Noor,Mohamed
-  Rana,Hamdy
-  NoUrhan,HaShem
-  Sara,Adel
-  Andy,Shenoda
-  Andy,Shenoda
-  Somaya,Emed
-  Rasha,Basha
-  Rasha,Basha
-  Logy,Waleed
-  Roka,S. Zaki
-  Hanan,Nona
-  Sa,MoRa
-  Śĥì,Màà
-  مها,اروي
-  Dina,Alaa
-  Aya,Ahmed
-  Aya,Ahmed
-  Amna,G. Mahmoud
-  Maria,Khaled
-  Has,Na
-  Has,Na
-  Eman,Hakim
-  Rahma,Seleim
-  Nada,FaRghali
-  Nermeen,Faried
-  Marmora,Boles
-  Khaled,Mohamed
-  Yassin Abdelrazek,Ahmed
-  Ahmed,Hegazy`;
-  
 
   const entries = data.split('\n');
   const dataArray = entries.map((entry) => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,42 +10,48 @@ import GenderComponent from "@/app/component/Gender";
 export default function Home() {
   return (
     <>
-      <div className="Name ">
-        <p id="p-NAME">
-          {" "}
-          NAME : <Names />
-        </p>
-        <p id="p-STUTAS">
-          {" "}
-          STUTUS : <Stats />{" "}
-        </p>
-        <p id="p-PHONE">
-          PHONE : <Phones />
-        </p>
-        <p id="p-CITY">
-          {" "}
-          CITY : <CityComponent />
-        </p>
-        <p id="p-GENDER">
-          GENDER : <GenderComponent />{" "}
-        </p>
+      <div className="Name p-4">
+        {/* NAME Section */}
+        <section id="nameSection" className="mb-8">
+          <h2 className="text-xl font-bold">NAME :</h2>
+          <p id="p-NAME" className="mt-2">
+            <Names />
+          </p>
+        </section>
+
+        {/* STATUS Section */}
+        <section id="statusSection" className="mb-8">
+          <h2 className="text-xl font-bold">STATUS :</h2>
+          <p id="p-STUTAS" className="mt-2">
+            <Stats />
+          </p>
+        </section>
+
+        {/* PHONE Section */}
+        <section id="phoneSection" className="mb-8">
+          <h2 className="text-xl font-bold">PHONE :</h2>
+          <p id="p-PHONE" className="mt-2">
+            <Phones />
+          </p>
+        </section>
+
+        {/* CITY Section */}
+        <section id="citySection" className="mb-8">
+          <h2 className="text-xl font-bold">CITY :</h2>
+          <p id="p-CITY" className="mt-2">
+            <CityComponent />
+          </p>
+        </section>
+
+        {/* GENDER Section */}
+        <section id="genderSection" className="mb-8">
+          <h2 className="text-xl font-bold">GENDER :</h2>
+          <p id="p-GENDER" className="mt-2">
+            <GenderComponent />
+          </p>
+        </section>
       </div>
     </>
   );
 }
 
-{
-  /* {section.nameSection !== 'nav' && section.nameSection !== 'footer' ? section.content : null} */
-}
-{
-  /* {section.nameSection !== 'nav' && section.nameSection !== 'footer' ? section.content : null} */
-}
-// <CustomSection
-//   key={index}
-//   height={section.height}
-//   className={`${section.nameSection} ${
-//     section.height == "8vh" ? "md:px-10" : ""
-//   }`}
-// >
-//   {section.content}
-// </CustomSection>
