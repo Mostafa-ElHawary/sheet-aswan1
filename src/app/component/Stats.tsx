9260fdebfea4b9d82328d8b2cf8f8329517dcd55
Mostafa-ElHawary <alhwarymostafa@gmail.com>
2023-12-28 22:26:28 +0200

    edding at Status and logic func data

--- src/app/component/Stats.tsx
+++ src/app/component/Stats.tsx
@@ -1,149 +1,238 @@
 "use client";
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const StatusComponent: React.FC = () => {
   const statuses = `Married
-Single
-Married
-Single
-Married
-Single
+  Single
+  Married
+  
+  Single
+  
+  
+  Married
+  Single
+  Married
+  
+  Single
+  
+  
+  
+  
+  
+  
+  Married
+  Married
+  Single
+  Single
+  
+  Single
+  
+  Married
+  
+  
+  
+  Single
+  Single
+  
+  
+  Married
+  
+  
+  
+  
+  Married
+  Married
+  Married
+  Single
+  Single
+  Single
+  
+  Single
+  Single
+  
+  
+  
+  
+  
+  Married
+  
+  
+  In an open relationship
+  In an open relationship
+  In an open relationship
+  In an open relationship
+  Single
+  Single
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  Single
+  Single
+  Single
+  Single
+  
+  
+  
+  
+  
+  
+  
+  
+  It's complicated
+  It's complicated
+  It's complicated
+  
+  
+  
+  
+  
+  
+  Single
+  Single
+  Single
+  
+  
+  
+  
+  Married
+  Married
+  Married
+  Married
+  Married
+  
+  
+  Married
+  Married
+  
+  Single
+  Single
+  
+  Engaged
+  Engaged
+  
+  
+  
+  
+  Single
+  
+  
+  Single
+  Engaged
+  Engaged
+  
+  
+  Married
+  Single
+  
+  Single
+  
+  
+  
+  
+  
+  
+  Single
+  
+  Single
+  Married
+  
+  
+  Single
+  
+  
+  
+  Engaged
+  Married
+  
+  
+  
+  
+  
+  
+  Married
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  Married
+  
+  Married
+  Single
+  Married
+  Single
+  
+  
+  
+  Single
+  Married
+  Married
+  Married
+  
+  
+  Single
+  Single
+  Married
+  Single
+  Single
+  Single
+  Single
+  
+  
+  
+  
+  Single
+  
+  Single
+  Single
+  Married
+  Married`
+  //   .split('\n')
+  //   .filter(Boolean)
+  //   .map((line) => line.trim());
 
-Married
-Single
-Married
-Single
+  // useEffect(() => {
+  //   const statusElement = document.getElementById('p-STUTAS');
 
-Single
-Married
+  //   if (statusElement) {
+  //     statuses.forEach((status, index) => {
+  //       const pElement = document.createElement('p');
+  //       pElement.innerText = `${index + 1} - ${status}`;
+  //       statusElement.appendChild(pElement);
+  //     });
+  //   }
+  // }, [statuses]);
 
-Single
-Single
+  // return null
+  .split('\n')
+  .filter(Boolean)
+  .map((line) => line.trim());
 
-Married
-Married
-Single
-Single
+const statusRef = useRef<HTMLParagraphElement | null>(null);
 
-Single
+useEffect(() => {
+  if (statusRef.current) {
+    statusRef.current.innerHTML = statuses
+      .map((status, index) => `${index + 1} - ${status}`)
+      .join('<br />');
+  }
+}, [statuses]);
 
-Married
-
-Single
-Single
-
-Married
-
-Married
-Married
-Single
-Single
-Single
-
-Single
-Single
-
-Married
-
-
-In an open relationship
-In an open relationship
-In an open relationship
-In an open relationship
-Single
-Single
-
-Single
-Single
-Single
-Single
-
-It's complicated
-It's complicated
-It's complicated
-
-Single
-Single
-Single
-
-Married
-Married
-Married
-Married
-Married
-
-Married
-Married
-
-Single
-Single
-
-Engaged
-Engaged
-
-Single
-
-Single
-Engaged
-Engaged
-
-Married
-Single
-
-Single
-
-Single
-
-Single
-Married
-
-Single
-
-Engaged
-Married
-
-Married
-
-Married
-
-Married
-
-Single
-Married
-Married
-Married
-
-Single
-Single
-Married
-Single
-Single
-Single
-Single
-
-Single
-
-Single
-Single
-Married
-Married`
-    .split('\n')
-    .filter(Boolean)
-    .map((line) => line.trim());
-
-  useEffect(() => {
-    const statusElement = document.getElementById('p-STUTAS');
-
-    if (statusElement) {
-      statuses.forEach((status, index) => {
-        const pElement = document.createElement('p');
-        pElement.innerText = `${index + 1} - ${status}`;
-        statusElement.appendChild(pElement);
-      });
-    }
-  }, [statuses]);
-
-  return null
+return <p id="p-STUTAS" ref={statusRef} className="mt-2 text-gray-800"></p>;
 };
 
 export default StatusComponent;
